d7a453d6361b202f7b4529d37671ae0d26ec8d35
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-04 11:14:07 +0100

    Finish OutputColors.java

--- src/main/java/at/bbrz/OutputColors.java
+++ src/main/java/at/bbrz/OutputColors.java
@@ -1,5 +1,9 @@
 package at.bbrz;
 
+import lombok.Getter;
+
+import java.security.InvalidParameterException;
+
 public enum OutputColors {
     RESET("\u001B[0m"),
     BLACK("\u001B[30m"),
@@ -9,9 +13,31 @@ public enum OutputColors {
     BLUE("\u001B[34m"),
     PURPLE("\u001B[35m");
 
-    public final String label;
+    @Getter
+    private final String label;
 
     OutputColors(String label) {
         this.label = label;
+    }
+
+    public static OutputColors findColor(String color) throws InvalidParameterException {
+        String colorUpperCase = color.toUpperCase();
+        OutputColors result = null;
+        for (OutputColors outputColor : values()) {
+            if (outputColor.name().equals(colorUpperCase)) {
+                result = outputColor;
+                break;
+            }
+        }
+
+        if (result == null || result == RESET) {
+            throw new InvalidParameterException(color + " is not a valid color!");
+        }
+
+        return result;
+    }
+
+    public static String getResetLabel() {
+        return RESET.label;
     }
 }
