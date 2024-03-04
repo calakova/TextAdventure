7068fba7303b1acf67683d2cf2a2c5161a66f825
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-04 11:12:54 +0100

    Finish (for now) CommandHandlerTest.java

--- src/test/java/at/bbrz/CommandHandlerTest.java
+++ src/test/java/at/bbrz/CommandHandlerTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.io.PrintStream;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -14,24 +13,102 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CommandHandlerTest {
-    @InjectMocks
-    CommandHandler commandHandlerMock;
-    @Mock
-    Runnable runnableMock;
+    CommandHandler commandHandler;
     @Mock
     Output outputMock;
     @Captor
     ArgumentCaptor<String> stringArgumentCaptor;
+    @Mock
+    Move moveMock;
+    @Mock
+    Exit exitMock;
 
-    // TODO: Fix this test!
+
+    @BeforeEach
+    void setup() {
+        commandHandler = new CommandHandler(outputMock);
+    }
+
     @Test
     public void runCommandInvalidInput() {
-        commandHandlerMock.setCommand("test");
+        commandHandler.setCommand("test");
 
-        commandHandlerMock.runCommand();
-        verify(outputMock, times(1))
-                .printLine(stringArgumentCaptor.capture());
-        assertEquals("Invalid command!", stringArgumentCaptor.getValue());
-        verify(outputMock, times(1)).emptyLine();
+        commandHandler.runCommand();
+        verify(outputMock).printLine(stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+        List<String> capturedStrings = stringArgumentCaptor.getAllValues();
+
+        assertEquals("Invalid command!", capturedStrings.get(0));
+        assertEquals("red", capturedStrings.get(1));
+        verify(outputMock).emptyLine();
+    }
+
+    @Test
+    public void runCommandEmptyStringInput() {
+        commandHandler.setCommand("");
+
+        commandHandler.runCommand();
+        verify(outputMock).printLine(stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+        List<String> capturedStrings = stringArgumentCaptor.getAllValues();
+
+        assertEquals("Invalid command!", capturedStrings.get(0));
+        assertEquals("red", capturedStrings.get(1));
+        verify(outputMock).emptyLine();
+    }
+
+    @Test
+    public void runCommandNullInput() {
+        commandHandler.setCommand(null);
+
+        commandHandler.runCommand();
+        verify(outputMock).printLine(stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+        List<String> capturedStrings = stringArgumentCaptor.getAllValues();
+
+        assertEquals("Invalid command!", capturedStrings.get(0));
+        assertEquals("red", capturedStrings.get(1));
+        verify(outputMock).emptyLine();
+    }
+
+    @Test
+    public void runCommandLongStringInput() {
+        commandHandler.setCommand("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        commandHandler.runCommand();
+        verify(outputMock).printLine(stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+        List<String> capturedStrings = stringArgumentCaptor.getAllValues();
+
+        assertEquals("Invalid command!", capturedStrings.get(0));
+        assertEquals("red", capturedStrings.get(1));
+        verify(outputMock).emptyLine();
+    }
+
+    @Test
+    public void runCommandSpecialCharactersInput() {
+        commandHandler.setCommand("!\"§²³@€$%&/()=?{[]}ß\\üäö<>|`´#'+*~,;.:-_^°");
+
+        commandHandler.runCommand();
+        verify(outputMock).printLine(stringArgumentCaptor.capture(), stringArgumentCaptor.capture());
+        List<String> capturedStrings = stringArgumentCaptor.getAllValues();
+
+        assertEquals("Invalid command!", capturedStrings.get(0));
+        assertEquals("red", capturedStrings.get(1));
+        verify(outputMock).emptyLine();
+    }
+
+    @Test
+    void runCommandValidInputMove() {
+        commandHandler.addCommand("move", moveMock);
+        commandHandler.setCommand("move");
+        commandHandler.runCommand();
+        verify(moveMock).run();
+    }
+
+    @Test
+    void runCommandValidInputExit() {
+        commandHandler.addCommand("exit", exitMock);
+        commandHandler.setCommand("exit");
+        commandHandler.runCommand();
+        verify(exitMock).run();
     }
 }
