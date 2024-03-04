7976cf0fd292bfd560c282d016df2052eb3f5773
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-04 11:58:02 +0100

    Finish (for now) Move.java

--- src/main/java/at/bbrz/Move.java
+++ src/main/java/at/bbrz/Move.java
@@ -1,38 +1,38 @@
 package at.bbrz;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
-public class Move implements Runnable {
+public class Move implements Command {
     private final Game game;
+    private Room currentRoom;
     private final OutputHandler outputHandler;
     private final InputHandler inputHandler;
-    private final Scanner scanner = new Scanner(System.in);
 
-    public Move(Game game) {
+    public Move(Game game, Room currentRoom, OutputHandler outputHandler, InputHandler inputHandler) {
         this.game = game;
-        this.outputHandler = game.getOutputHandler();
-        this.inputHandler = game.getInputHandler();
+        this.currentRoom = currentRoom;
+        this.outputHandler = outputHandler;
+        this.inputHandler = inputHandler;
     }
 
     @Override
     public void run() {
-        while (true) {
-            String[] inputAndAvailableDirections = askDirection();
-            String input = inputAndAvailableDirections[0];
-            String availableDirections = inputAndAvailableDirections[1];
-            if (isDirectionValid(input, availableDirections)) {
-                changeRoom(input);
-                break;
-            }
-            outputHandler.printLine("Invalid direction!");
+        String[] inputAndAvailableDirections = askDirection();
+        String input = inputAndAvailableDirections[0];
+        String availableDirections = inputAndAvailableDirections[1];
+
+        if (isDirectionValid(input, availableDirections)) {
+            changeRoom(input);
+            return;
         }
+        outputHandler.printLine("Invalid direction!", "red");
+        outputHandler.emptyLine();
     }
 
     private String[] askDirection() {
-        String directions = game.getCurrentRoom().getExitDirections();
-        outputHandler.printLine("Which direction do you want to go in?\n" +
-                "Available directions: " + directions);
+        String directions = currentRoom.getExitDirections();
+        outputHandler.printLine("Which direction do you want to go in?", "yellow");
+        outputHandler.printLine("Available directions: " + directions, "blue");
         String input = inputHandler.getNextLine();
         outputHandler.emptyLine();
         return new String[]{input, directions};
@@ -43,8 +43,11 @@ public class Move implements Runnable {
         return Arrays.asList(directionArr).contains(input);
     }
 
+    /* TODO: Maybe find a way to not have the same property in two separate classes
+    (assuming that doing so doesn't break unit tests */
     private void changeRoom(String direction) {
-        Room nextRoom = game.getCurrentRoom().getExits().get(direction);
+        Room nextRoom = currentRoom.getExitFor(direction);
         game.setCurrentRoom(nextRoom);
+        this.currentRoom = nextRoom;
     }
 }
