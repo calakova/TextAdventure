f584a49e68631fb495306f65285b3815dd396719
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-04 11:11:48 +0100

    Finish ExitTest.java

--- src/test/java/at/bbrz/ExitTest.java
+++ src/test/java/at/bbrz/ExitTest.java
@@ -1,2 +1,24 @@
-package at.bbrz;public class ExitTest {
+package at.bbrz;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ExitTest {
+    @InjectMocks
+    Exit exit;
+    @Mock
+    Game game;
+
+    @Test
+    void runDisablesGameLoop() {
+        exit.run();
+        verify(game, times(1)).setGameRunning(false);
+    }
 }
